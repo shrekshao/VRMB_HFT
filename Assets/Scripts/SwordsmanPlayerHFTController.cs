ffb43a8c8c6263306b38a512719c8cdfccc3f831
shrekshao <shrekshao@gmail.com>
2017-02-25 14:11:21 -0500

    ikhandler swing sword

---
+++ Assets/Scripts/SwordsmanPlayerHFTController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordsmanPlayerHFTController : MonoBehaviour {
+
+    //[SerializeField]
+    //public Transform sword;
+    //[SerializeField]
+    //public Transform ikHandler;
+
+    private Transform sword;
+    private Transform ikHandler;
+    private Transform hand;
+
+    public float rotationSpeed = 5.0f;
+    public float moveSpeed = 5.0f;
+    public float moveFriction = 0.95f;
+
+
+    private HFTGamepad m_gamepad;
+    private HFTInput m_hftInput;
+
+    // TODO: camera base rotation
+    private Quaternion inverseBaseRotation;     // base quaternion of controller (used for swords)
+    private Vector3 baseEulerAngles;            // base euler angles of controller (used for ikhandler)
+    //private Quaternion lastRotation;
+
+    private static int s_playerCount = 0;
+
+    // Use this for initialization
+    void Start () {
+        m_gamepad = GetComponent<HFTGamepad>();
+        m_hftInput = GetComponent<HFTInput>();
+
+        ikHandler = transform.FindChild("IkHandler");
+        //sword = ikHandler.FindChild("SwordParent");
+        sword = transform.FindChild("SwordParent");
+        hand = ikHandler.FindChild("Hand");
+
+        int playerNdx = s_playerCount++;
+
+
+        SetColor(m_gamepad.Color);
+
+
+        // Delete ourselves if disconnected
+        m_gamepad.OnDisconnect += Remove;
+
+
+        //baseRotation = Quaternion.identity;
+        inverseBaseRotation = Quaternion.identity;
+        //lastRotation = transform.rotation;
+    }
+
+
+    void Remove()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotation
+        if (m_hftInput.GetButtonDown("fire1"))
+        {
+            // adjust sword 
+            inverseBaseRotation = Quaternion.Inverse(m_hftInput.gyro.attitude);
+            baseEulerAngles = m_hftInput.gyro.attitude.eulerAngles;
+            sword.rotation = Quaternion.identity;
+        }
+        else
+        {
+            
+            Quaternion q = m_hftInput.gyro.attitude;
+            sword.rotation = inverseBaseRotation * q;
+
+
+
+            Vector3 deltaEulerAngles = q.eulerAngles - baseEulerAngles;
+
+            ikHandler.eulerAngles = q.eulerAngles - baseEulerAngles;
+        }
+
+        sword.position = hand.position;
+
+    }
+
+
+
+    //private float CenterOut(int v)
+    //{
+    //    if (v == 0)
+    //    {
+    //        return (float)v;
+    //    }
+    //    return (float)((v + 1) / 2 * ((v & 1) == 0 ? 1 : -1));
+    //}
+
+    private void SetColor(Color color)
+    {
+        //m_color = color;
+        //m_renderer.material.color = m_color;
+        //Color[] pix = new Color[1];
+        //pix[0] = color;
+        //Texture2D tex = new Texture2D(1, 1);
+        //tex.SetPixels(pix);
+        //tex.Apply();
+        //m_guiStyle.normal.background = tex;
+    }
+}
