dba0dea81c47dd04e98e83e607e6a1acdd6f2199
shrekshao <shrekshao@gmail.com>
2017-08-19 19:34:13 -0700

    look at boss cam

---
+++ Assets/Scripts/BossBattle/CameraLookAt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour {
+
+    public Transform lookAtTarget;
+
+    private Vector3 tmpPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tmpPosition = lookAtTarget.position;
+        tmpPosition.y = transform.position.y;
+        transform.LookAt(tmpPosition);
+	}
+}
